aab121b26610f8247e5c50c3e24e7518f649b3d4
Tchaka Spierings <tchaka.spierings@gmail.com>
2020-04-26 12:35:27 +0200

    Add waveform-synthesizer component

---
+++ src/components/waveform-synthesizer/waveform-synthesizer.tsx
@@ -0,0 +1,8 @@
+import './waveform-synthesizer.scss';
+import * as React from 'react';
+
+export class WaveformSynthesizer extends React.Component<{}, {}> {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
