b25d0b4cd63e12c0aca3aed3723a901e56b9e59c
Tchaka Spierings <tchaka.spierings@gmail.com>
2020-04-26 12:48:16 +0200

    Visually layout basic component structure

--- src/components/waveform-synthesizer/waveform-synthesizer.tsx
+++ src/components/waveform-synthesizer/waveform-synthesizer.tsx
@@ -3,6 +3,11 @@ import * as React from 'react';
 
 export class WaveformSynthesizer extends React.Component<{}, {}> {
   render() {
-    return <h1>Hello</h1>;
+    return <div className='waveform-synthesizer'>
+      <div className='master'><label>Master mix bar placeholder</label></div>
+      <div className='waveform-editor'><label>Waveform editor placeholder</label></div>
+      <div className='frequency-graph'><label>Frequency graph placeholder</label></div>
+      <div className='midi-keyboard'><label>Midi keyboard placeholder</label></div>
+    </div>;
   }
 }
