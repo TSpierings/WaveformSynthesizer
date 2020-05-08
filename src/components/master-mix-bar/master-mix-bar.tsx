ecdf049763019244557865875e74d597b86fc9f5
Tchaka Spierings <tchaka.spierings@gmail.com>
2020-05-08 13:05:38 +0200

    Add master mix bar with title and master gain knob

---
+++ src/components/master-mix-bar/master-mix-bar.tsx
@@ -0,0 +1,17 @@
+import './master-mix-bar.scss'
+import * as React from 'react';
+import { Knob } from 'components/shared/knob/knob';
+
+export interface MasterMixBarProps {
+  onMasterGainChange: Function
+}
+
+export class MasterMixBar extends React.Component<MasterMixBarProps, {}> {
+
+  render() {
+    return <div className='master-mix-bar'>
+      <h1>Waveform Synthesizer</h1>
+      <Knob min={0} max={100} default={50} name='master' onValueChange={this.props.onMasterGainChange}/>
+    </div>
+  }
+}
